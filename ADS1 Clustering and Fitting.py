137662418a12c95795a285db9f6bd249f8dfaa9d
Girijprasad Janappa Gherade <girijjgherade1718@gmail.com>
2024-12-11 22:17:47 +0000

    Update ADS1 Clustering and Fitting.py

--- ADS1 Clustering and Fitting.py
+++ ADS1 Clustering and Fitting.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 import seaborn as sns
 
 # Load the dataset
-file_path = "Adidas Vs Nike.csv"
+file_path = r"C:\Users\girij\Downloads\Clustering\Adidas Vs Nike.csv"
 data = pd.read_csv(file_path)
 
 # Inspect the first few rows of the dataset
